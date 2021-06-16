7a9577f9066b77e589ea8dcb42fec44c8b4fb044
Nandan Patil <tryanother609@gmail.com>
2021-06-16 11:50:02 +0000

    version 0.3.2

---
+++ inst/doc/dm2.R
@@ -0,0 +1,39 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+# Load the package
+library(gpbStat)
+
+#Load the dataset
+data(dm2rcbd)
+
+# View the structure of dataframe. 
+str(dm2rcbd)
+
+# Conduct Line x Tester analysis
+result = dm2(dm2rcbd, rep, parent1, parent2, DTP)
+
+# View the output
+result
+
+
+## -----------------------------------------------------------------------------
+# Load the package
+library(gpbStat)
+
+#Load the dataset
+data(dm2alpha)
+
+# View the structure of dataframe. 
+str(dm2alpha)
+
+# Conduct Line x Tester analysis
+result1 = dm2(dm2alpha, rep, parent1, parent2, TW, blk)
+
+# View the output
+result1
+
